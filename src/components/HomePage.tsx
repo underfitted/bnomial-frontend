af4a67bea80371be3b37c773154121ee0169d8ab
Vladimir Haltakov <vladimir.haltakov@gmail.com>
2022-01-26 23:56:46 +0100

    Remove unneeded imports

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,12 +1,5 @@
-import React from "react";
-import { Question as QuestionType } from "../types";
-import { useApiClient } from "../api";
-import Question from "./Question";
-
 const HomePage = () => {
-
-
-  return <h1>Welcome to Bnomial!</h1>;
+    return <h1>Welcome to Bnomial!</h1>;
 };
 
 export default HomePage;
