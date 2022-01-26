bfbd6e20f3b7d1f746902639221abf974c0ab1a9
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2022-01-26 22:30:01 +0900

    Add Placeholder pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
+import PlaceholderPage from "./components/PlaceholderPage";
 import PageLayout from "./components/PageLayout";
 
 import { pathForPage } from "./pages";
@@ -10,6 +11,10 @@ function App() {
       <Routes>
         <Route element={<PageLayout/>}>
           <Route path={pathForPage('home')} element={<Home/>} />
+          <Route path={pathForPage('leaderboard')} element={<PlaceholderPage title='Leaderboard' />} />
+          <Route path={pathForPage('yesterdays')} element={<PlaceholderPage title='Yesterdays' />} />
+          <Route path={pathForPage('how_it_works')} element={<PlaceholderPage title='How it works' />} />
+          <Route path={pathForPage('contribute')} element={<PlaceholderPage title='Contribute' />} />
         </Route>
       </Routes>
     </BrowserRouter>
