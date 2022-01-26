ecc56d946bf89d42613709203871394523ac2049
Vladimir Haltakov <vladimir.haltakov@gmail.com>
2022-01-26 23:10:05 +0100

    Move the question from Home to the Quiz page

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./components/HomePage";
 import PlaceholderPage from "./components/PlaceholderPage";
 import PageLayout from "./components/PageLayout";
+import Quiz from "./components/Quiz";
 
 import { ApiContext, DefaultApiClient } from "./api";
 import { pathForPage } from "./pages";
@@ -13,7 +14,7 @@ function App() {
         <Routes>
           <Route element={<PageLayout />}>
           <Route path={pathForPage('home')} element={<HomePage/>} />
-          <Route path={pathForPage('quiz')} element={<PlaceholderPage title='Quiz' />} />
+          <Route path={pathForPage('quiz')} element={<Quiz />} />
           <Route path={pathForPage('how_it_works')} element={<PlaceholderPage title='How it works' />} />
           <Route path={pathForPage('leaderboard')} element={<PlaceholderPage title='Leaderboard' />} />
           <Route path={pathForPage('practice')} element={<PlaceholderPage title='Practice' />} />
--- src/components/Question.tsx
+++ src/components/Question.tsx
@@ -12,25 +12,31 @@ import { Question as QuestionType } from '../types';
 function Question({ question }: { question?: QuestionType }) {
   return (
     <>
-        <QuestionNumberContainer>
-          QUESTION
-          <QuestionNumber>0013</QuestionNumber>
-        </QuestionNumberContainer>
-      <QuestionTitle>{question?.title}</QuestionTitle>
-      <QuestionDescription>{question?.content}</QuestionDescription>
+      <QuestionNumberContainer>
+        QUESTION
+        <QuestionNumber>0013</QuestionNumber>
+      </QuestionNumberContainer>
+
+      <QuestionTitle>Capital of Spain</QuestionTitle>
+      <QuestionDescription>Marry is traveling to Spain for her vacation. She is wondering which of the following cities is the capital of Spain?</QuestionDescription>
+      
+      <QuestionChoice>Habana</QuestionChoice>
+      <QuestionChoice>Miami</QuestionChoice>
+      <QuestionChoice>Madrid</QuestionChoice>
+
+      <QuestionButton>SUBMIT MY ANSWER</QuestionButton>
+      <QuestionButton>I'M DONE</QuestionButton>
+
       {
       /*
+      <QuestionTitle>{question?.title}</QuestionTitle>
+      <QuestionDescription>{question?.content}</QuestionDescription>
       Object.keys(question?.choices).map(function(key) {
         return <QuestionChoice key={key}>{question?.choices[key]}</QuestionChoice>;
       })
       */
       }
-      <QuestionChoice>Habana</QuestionChoice>
-      <QuestionChoice>Miami</QuestionChoice>
-      <QuestionChoice>Madrid</QuestionChoice>
-      <QuestionButton>SUBMIT MY ANSWER</QuestionButton>
-      <QuestionButton>I'M DONE</QuestionButton>
-    </>
+    </>    
   );
 }
 
