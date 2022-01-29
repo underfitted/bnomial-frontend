a48c525efdd772f9a5a9613b1d1a3d44a195311c
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2022-01-29 11:36:44 +0900

    Refactor test

--- src/components/Question.test.tsx
+++ src/components/Question.test.tsx
@@ -1,24 +1,31 @@
 import { render, screen } from "@testing-library/react";
 
+import { Question as QuestionType } from "../types";
 import Question from "./Question";
 
 describe("Question", () => {
     test("shows the title and description", () => {
-        const question = {
-            id: 'abc',
+        const question = buildQuestion({
             title: "What is the capital of France?",
             content: "The capital of France is Paris.",
-            choices: {
-                A: "Paris",
-                B: "London",
-                C: "Berlin",
-                D: "Madrid",
-            },
-        };
+        });
 
         render(<Question question={question} />);
 
         expect(screen.getByText(question.title)).toBeInTheDocument();
         expect(screen.getByText(question.content)).toBeInTheDocument();
     });
+});
+
+const buildQuestion = (values?: Partial<QuestionType>): QuestionType => ({
+    id: 'abc',
+    title: "What is the capital of France?",
+    content: "The capital of France is Paris.",
+    choices: {
+        A: "Paris",
+        B: "London",
+        C: "Berlin",
+        D: "Madrid",
+    },
+    ...(values || {}),
 });
