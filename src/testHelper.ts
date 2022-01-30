c63e923b9802611eda686a259f1b25dc9bcac85d
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2022-01-30 17:43:03 +0900

    Make factory produce different ids each time

--- src/testHelper.ts
+++ src/testHelper.ts
@@ -1,7 +1,15 @@
 import { Question } from "./types";
 
+let nextId = 0;
+
+const getNextId = (): number => {
+    const value = nextId;
+    nextId += 1;
+    return value;
+};
+
 export const buildQuestion = (values?: Partial<Question>): Question => ({
-    id: "abc",
+    id: `question-${getNextId()}`,
     title: "What is the capital of France?",
     content: "The capital of France is Paris.",
     choices: {
