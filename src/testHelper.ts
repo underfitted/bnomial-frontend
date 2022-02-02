44903a945633ce4ca6fa97e08b542b11bb5015c9
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2022-02-03 07:11:43 +0900

    Simplify id generation in factory

--- src/testHelper.ts
+++ src/testHelper.ts
@@ -3,14 +3,8 @@ import { Question } from "./types";
 
 let nextId = 0;
 
-const getNextId = (): number => {
-    const value = nextId;
-    nextId += 1;
-    return value;
-};
-
 export const buildQuestion = (values?: Partial<Question>): Question => ({
-    id: `question-${getNextId()}`,
+    id: `question-${nextId++}`,
     title: "What is the capital of France?",
     content: "The capital of France is Paris.",
     choices: {
