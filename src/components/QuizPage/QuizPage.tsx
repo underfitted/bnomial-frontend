089190497f43785b90855424e82105c9aed6ec08
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2022-02-03 01:35:01 +0900

    Rewrite QuizPage with the hook

--- src/components/QuizPage/QuizPage.tsx
+++ src/components/QuizPage/QuizPage.tsx
@@ -1,50 +1,12 @@
 import React from "react";
-import { Question as QuestionType } from "../../types";
-import { useApiClient } from "../../api";
-import QuizDisplay, { QuizHandler } from "./QuizDisplay";
+import QuizDisplay from "./QuizDisplay";
+import useSampleQuizHandler from "./useSampleQuizHandler";
 
 const QuizPage = () => {
-    const apiClient = useApiClient();
-    const [sampleQuestions, setSampleQuestions] = React.useState<QuestionType[]>();
+    const sampleQuizHandler = useSampleQuizHandler();
+    // TODO: Use different handler for logged in user
 
-    React.useEffect(() => {
-        (async () => {
-            const sampleQuestions = await apiClient.getSampleQuestions();
-            setSampleQuestions(sampleQuestions);
-        })();
-    }, [apiClient]);
-
-    const quizHandler = React.useMemo<QuizHandler>(() => {
-        const answers: { [questionId: string]: string } = {}; // TODO: Get from cookie
-
-        return {
-            getNextQuestion: async () => {
-                if (!sampleQuestions) {
-                    return { type: "not_started" };
-                }
-
-                const unansweredQuestions = sampleQuestions.filter(question => answers[question.id] === undefined);
-                if (unansweredQuestions.length === 0) {
-                    return { type: "no_more_questions" };
-                }
-
-                const randomIndex = Math.floor(Math.random() * unansweredQuestions.length);
-                const question = unansweredQuestions[randomIndex];
-
-                return { type: "question", question };
-            },
-            answerQuestion: async (questionId, answer) => {
-                answers[questionId] = answer;
-                // TODO: Save to cookie
-            },
-        };
-    }, [sampleQuestions]);
-
-    if (!sampleQuestions) {
-        return <div>Loading...</div>;
-    }
-
-    return <QuizDisplay quizHandler={quizHandler} />;
+    return <QuizDisplay quizHandler={sampleQuizHandler} />;
 };
 
 export default QuizPage;
