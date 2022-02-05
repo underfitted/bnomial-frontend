7b37b4bfcc021cb6e9d7fb560f73d58d904d74f2
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2022-02-05 21:22:24 +0900

    Reorganize implementation

--- scripts/generate-api-client.ts
+++ scripts/generate-api-client.ts
@@ -1,9 +1,22 @@
 import { spawnSync } from "child_process";
 
-const GENERATED_FILEPATH = 'src/api/generatedClient.ts';
+const main = () => {
+    runNpxCommand(`oazapfts ./bnomial/openapi.yml ${GENERATED_FILEPATH} --optimistic`);
+
+    const { stdout } = spawnSync("git", ["status"]);
+
+    if (stdout.toString().includes(GENERATED_FILEPATH)) {
+        console.error(
+            `${GENERATED_FILEPATH} is not up to date. Please run "npm run generate-api-client" and commit the changes.`
+        );
+        process.exit(1);
+    }
+};
+
+const GENERATED_FILEPATH = "src/api/generatedClient.ts";
 
 const runNpxCommand = (command: string) => {
-    const { status } = spawnSync("npx", command.split(' '), {
+    const { status } = spawnSync("npx", command.split(" "), {
         stdio: "inherit",
     });
     if (status !== 0) {
@@ -11,11 +24,6 @@ const runNpxCommand = (command: string) => {
     }
 };
 
-runNpxCommand(`oazapfts ./bnomial/openapi.yml ${GENERATED_FILEPATH} --optimistic`);
-
-const { stdout } = spawnSync("git", ["status"]);
-
-if (stdout.toString().includes(GENERATED_FILEPATH)) {
-  console.error(`${GENERATED_FILEPATH} is not up to date. Please run "npm run generate-api-client" and commit the changes.`);
-  process.exit(1);
+if (require.main === module) {
+    main();
 }
