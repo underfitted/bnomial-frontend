10c61cf20a1f8e383cfed52029840b923763bd28
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2022-02-05 18:46:50 +0900

    Return error if generated client is not up to date

--- scripts/generate-api-client.ts
+++ scripts/generate-api-client.ts
@@ -1,9 +1,18 @@
 import { spawnSync } from "child_process";
 
+const GENERATED_FILEPATH = 'src/api/generatedClient.ts';
+
 const runNpxCommand = (command: string) => {
     spawnSync("npx", command.split(' '), {
         stdio: "inherit",
     });
 };
 
-runNpxCommand("oazapfts ./bnomial/openapi.yml ./src/api/generatedClient.ts --optimistic");
+runNpxCommand(`oazapfts ./bnomial/openapi.yml ${GENERATED_FILEPATH} --optimistic`);
+
+const { stdout } = spawnSync("git", ["status"]);
+
+if (stdout.toString().includes(GENERATED_FILEPATH)) {
+  console.error(`${GENERATED_FILEPATH} is not up to date. Please run "npm run generate-api-client" and commit the changes.`);
+  process.exit(1);
+}
